f51615cfbd33c7df02e440966159ae4f87d5a422
QuantumAutoForge <yoyomohitkumarsir@gmail.com>
2025-02-25 19:11:59 +0530

    Update post_meme.py

--- post_meme.py
+++ post_meme.py
@@ -7,12 +7,22 @@ from io import BytesIO
 
 POSTED_MEMES_FILE = "posted_memes.json"
 
+# 🔹 Twitter API Setup
 client = tweepy.Client(
     consumer_key=os.getenv("TWITTER_API_KEY"),
     consumer_secret=os.getenv("TWITTER_API_SECRET"),
     access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
     access_token_secret=os.getenv("TWITTER_ACCESS_SECRET")
 )
+
+# 🔹 Tweepy v4+ Requires `API` for Media Upload
+auth = tweepy.OAuth1UserHandler(
+    consumer_key=os.getenv("TWITTER_API_KEY"),
+    consumer_secret=os.getenv("TWITTER_API_SECRET"),
+    access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
+    access_token_secret=os.getenv("TWITTER_ACCESS_SECRET")
+)
+api = tweepy.API(auth)
 
 def load_memes():
     if os.path.exists("approved_memes.json"):
@@ -32,11 +42,14 @@ def post_meme():
     img = Image.open(BytesIO(response.content))
     img.save("meme.jpg")
 
-    media = client.media_upload(filename="meme.jpg")
+    # 🔹 Upload media using `API` instead of `Client`
+    media = api.media_upload(filename="meme.jpg")
+
+    # 🔹 Post Tweet using `Client`
     tweet_text = f"{meme['title']}\n\n🔗 Source: {meme['permalink']}"
+    tweet = client.create_tweet(text=tweet_text, media_ids=[media.media_id_string])
 
-    tweet = client.create_tweet(text=tweet_text, media_ids=[media.media_id])
-
+    # 🔹 Save remaining memes
     with open("approved_memes.json", "w") as f:
         json.dump(memes, f, indent=4)
 
